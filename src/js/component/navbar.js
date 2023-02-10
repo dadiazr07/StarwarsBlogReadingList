48d1c682e4d8c10878aae7f3f95b07258672a898
dadiazr07 <dadiazr.07@gmail.com>
2023-02-10 23:39:51 +0000

    Nav and font

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,15 +3,12 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
+
+			<nav class="navbar navbar-dark bg-dark navbar-expand-lg sticky-top">
+				<div class="container-fluid">
+				<a class="navbar-brand text-warning" href="#">Navbar</a>
+				</div>
+			</nav>
+
 	);
 };
