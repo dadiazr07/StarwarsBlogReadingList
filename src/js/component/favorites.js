9c66b0e2f08697f65c726c685ceab497f5a1adf7
dadiazr07 <dadiazr.07@gmail.com>
2023-03-19 17:05:46 -0400

    dropdown

--- src/js/component/favorites.js
+++ src/js/component/favorites.js
@@ -5,11 +5,20 @@ export const Favorites = () => {
     const { store, actions } = useContext(Context)
     return (
         <>
-            <ul className="list-group list-group-flush list-unstyled p-0 mb-0 bg-light">
-            {store.favorites.map((element) => {
-                return <li key={element.id}>{element.name}</li>
-            })}
-            </ul>
+            <div class="btn-group dropstart ms-5">
+                <button type="button" class="btn btn-warning btn-lg dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                    Favoritos
+                </button>
+                <ul className="dropdown-menu ">
+                    <li><h6 class="dropdown-header">Tus favoritos</h6></li>
+                    <li><a class="dropdown-item" href="#">algo</a></li>
+                    <li><a class="dropdown-item" href="#">otra</a></li>
+                    <li><a class="dropdown-item" href="#">mas</a></li>
+                    {/* {store.favorites.map((element) => {
+                        return <li key={element.id}>{element.name}</li>
+                    })} */}
+                </ul>
+            </div>
         </>
     )
 }
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,12 +5,12 @@ import { Favorites } from "./favorites";
 export const Navbar = () => {
 	return (
 
-			<nav className="navbar mt-auto navbar-black bg-black navbar-expand-lg sticky-top">
-				<div className="container-fluid justify-content-center">
-				<Link to={"/"} className="navbar-brand text-warning ms-5" href="#"><h1 style={{ fontSize: 100}}>Star wars</h1></Link>
-				<Favorites/>
-				</div>
-			</nav>
+		<nav className="navbar mt-auto navbar-black bg-black navbar-expand-lg sticky-top">
+			<div className="container flex me-0">
+				<Link to={"/"} className="navbar-brand text-warning me-5" href="#"><h1 style={{ fontSize: 100 }}>Star wars</h1></Link>
+				<Favorites />
+			</div>
+		</nav>
 
 	);
 };
